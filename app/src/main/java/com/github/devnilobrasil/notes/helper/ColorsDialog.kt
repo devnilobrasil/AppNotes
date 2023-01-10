f244b5ede8e57b5cefe66263a20eb06f0a00289d
Nilo Sousa <dev.nilobrasil@gmail.com>
2023-01-10 16:55:39 +0000

    Inserção do Dialog Fragment para customizar o layout da dialog das cores das notas.

---
+++ app/src/main/java/com/github/devnilobrasil/notes/helper/ColorsDialog.kt
@@ -0,0 +1,107 @@
+package com.github.devnilobrasil.notes.helper
+
+import android.os.Bundle
+import android.view.*
+import androidx.core.content.ContextCompat
+import com.github.devnilobrasil.notes.helper.DatabaseConstants.COLORS.IdColors
+import androidx.fragment.app.DialogFragment
+import com.github.devnilobrasil.notes.R
+import com.github.devnilobrasil.notes.databinding.ColorsNoteLayoutBinding
+import com.github.devnilobrasil.notes.model.NotesModel
+
+class ColorsDialog : DialogFragment()
+{
+    private val binding: ColorsNoteLayoutBinding by lazy { ColorsNoteLayoutBinding.inflate(layoutInflater) }
+
+    private val notesModel = NotesModel()
+
+    var colorChoice = notesModel.color
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View
+    {
+        return binding.root.let {
+            if (it.parent != null) {
+                (it.parent as ViewGroup).removeView(it)
+            }
+            it
+        }
+    }
+
+    override fun onStart()
+    {
+        super.onStart()
+        dialog?.window?.setLayout(
+            WindowManager.LayoutParams.WRAP_CONTENT,
+            WindowManager.LayoutParams.WRAP_CONTENT
+        )
+        dialog?.window?.setBackgroundDrawableResource(android.R.color.transparent)
+
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?)
+    {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.colorDefault.setOnClickListener {
+            colorChoice = IdColors.DEFAULT
+            statusBarColor(colorChoice)
+            dismiss()
+        }
+
+        binding.colorRed.setOnClickListener {
+            colorChoice = IdColors.RED
+            statusBarColor(colorChoice)
+            dismiss()
+        }
+
+        binding.colorPurple.setOnClickListener {
+            colorChoice = IdColors.PURPLE
+            statusBarColor(colorChoice)
+            dismiss()
+        }
+
+        binding.colorGreen.setOnClickListener {
+            colorChoice = IdColors.GREEN
+            statusBarColor(colorChoice)
+            dismiss()
+        }
+
+        binding.colorGrey.setOnClickListener {
+            colorChoice = IdColors.GREY
+            statusBarColor(colorChoice)
+            dismiss()
+        }
+
+        binding.colorOrange.setOnClickListener {
+            colorChoice = IdColors.ORANGE
+            statusBarColor(colorChoice)
+            dismiss()
+        }
+
+        binding.colorIndigo.setOnClickListener {
+            colorChoice = IdColors.INDIGO
+            statusBarColor(colorChoice)
+            dismiss()
+        }
+
+        binding.colorYellow.setOnClickListener {
+            colorChoice = IdColors.YELLOW
+            statusBarColor(colorChoice)
+            dismiss()
+        }
+
+    }
+
+    private fun statusBarColor(color: Int){
+        val window: Window = requireActivity().window
+        window.statusBarColor = ContextCompat.getColor(requireContext(), color)
+        window.findViewById<View>(R.id.topAppBar).setBackgroundColor(ContextCompat.getColor(requireContext(), color))
+    }
+
+
+}
--- app/src/main/java/com/github/devnilobrasil/notes/ui/AddNotesFragment.kt
+++ app/src/main/java/com/github/devnilobrasil/notes/ui/AddNotesFragment.kt
@@ -3,16 +3,14 @@ package com.github.devnilobrasil.notes.ui
 import android.os.Bundle
 import android.view.*
 import android.widget.Toast
-import androidx.appcompat.app.AlertDialog
 import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.findNavController
 import com.github.devnilobrasil.notes.R
 import com.github.devnilobrasil.notes.databinding.FragmentAddNotesBinding
+import com.github.devnilobrasil.notes.helper.ColorsDialog
 import com.github.devnilobrasil.notes.helper.DatabaseConstants
-import com.github.devnilobrasil.notes.helper.DatabaseConstants.COLORS.IdColors
-import com.github.devnilobrasil.notes.helper.DatabaseConstants.COLORS.NameColors
 import com.github.devnilobrasil.notes.model.NotesModel
 import com.github.devnilobrasil.notes.viewmodels.NotesViewModel
 import com.google.android.material.appbar.MaterialToolbar
@@ -27,13 +25,14 @@ class AddNotesFragment : Fragment()
             layoutInflater
         )
     }
+
     private lateinit var notesViewModel: NotesViewModel
 
     private lateinit var topAppBar: MaterialToolbar
 
     private var noteId = 0
 
-    private var colorChoice = NotesModel().color
+    private val dialog : ColorsDialog = ColorsDialog()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -56,18 +55,17 @@ class AddNotesFragment : Fragment()
             binding.editBodyNotes.setText(it.body)
             binding.buttonSendNotes.text = getString(R.string.button_update)
             binding.topAppBar.title = getString(R.string.update_note_top_bar)
-            binding.topAppBar.setBackgroundColor(ContextCompat.getColor(requireContext(), it.color))
+            dialog.colorChoice = it.color
             statusBarColor(it.color)
         }
 
         notesViewModel.saveNotes.observe(viewLifecycleOwner) {
             if (it != "")
             {
-                Snackbar.make(
-                    requireContext(),
+                Snackbar.make(requireContext(),
                     binding.buttonSendNotes,
-                    it, Snackbar.LENGTH_SHORT
-                )
+                    it,
+                    Snackbar.LENGTH_SHORT)
                     .setAnchorView(binding.buttonSendNotes)
                     .setBackgroundTint(resources.getColor(R.color.teal_100, null))
                     .setTextColor(resources.getColor(R.color.black, null))
@@ -104,7 +102,7 @@ class AddNotesFragment : Fragment()
                     this.id = noteId
                     this.title = titleNote
                     this.body = bodyNote
-                    this.color = colorChoice
+                    this.color = dialog.colorChoice
 
                 }
                 notesViewModel.saveNotesDB(notesModel, requireContext())
@@ -142,42 +140,7 @@ class AddNotesFragment : Fragment()
                 }
                 R.id.palette ->
                 {
-
-                    val colors = arrayOf(
-                        NameColors.DEFAULT,
-                        NameColors.RED,
-                        NameColors.PURPLE,
-                        NameColors.GREEN,
-                        NameColors.GREY,
-                        NameColors.ORANGE,
-                        NameColors.INDIGO,
-                        NameColors.YELLOW
-                    )
-
-                    val builder = AlertDialog.Builder(requireContext())
-                    with(builder) {
-                        setTitle(getString(R.string.colors))
-                        setItems(colors)
-                        { _, which ->
-                            when (which)
-                            {
-                                0 -> colorChoice = IdColors.DEFAULT
-                                1 -> colorChoice = IdColors.RED
-                                2 -> colorChoice = IdColors.PURPLE
-                                3 -> colorChoice = IdColors.GREEN
-                                4 -> colorChoice = IdColors.GREY
-                                5 -> colorChoice = IdColors.ORANGE
-                                6 -> colorChoice = IdColors.INDIGO
-                                7 -> colorChoice = IdColors.YELLOW
-
-                            }
-                            topAppBar.setBackgroundColor(ContextCompat.getColor(requireContext(), colorChoice))
-                            statusBarColor(colorChoice)
-                        }
-                        create()
-                        show()
-                      }
-
+                    dialog.show(childFragmentManager, dialog.tag)
                     true
                 }
                 R.id.share ->
@@ -191,9 +154,11 @@ class AddNotesFragment : Fragment()
         }
 
     }
+
     private fun statusBarColor(color: Int){
         val window: Window = requireActivity().window
         window.statusBarColor = ContextCompat.getColor(requireContext(), color)
+        topAppBar.setBackgroundColor(ContextCompat.getColor(requireContext(), color))
     }
 
 }
